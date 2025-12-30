6024fc5f24d7393d7a1fdfddd9d86b6fe102fc02
Jakob <jakob.raidl@student.tugraz.at>
2025-12-30 16:46:31 +0100

    site v0.10

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: J0hAVoGk */
+/* Manifest version: JAkrjLea */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
